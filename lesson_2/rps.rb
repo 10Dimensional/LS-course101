936193f1fba75d082fd5240f34f0d14ab66a634d
Sloane Smith <sloane.smith@asu.edu>
2016-03-10 23:37:47 +0000

    Work up to exercises easy 3

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,13 +1,17 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') || (player == 'paper' && computer == 'rock') || (player == 'scissors' && computer == 'paper')
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') || (first == 'paper' && second == 'rock') || (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer) 
+  if win?(player, computer)
     prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') || (player == 'paper' && computer == 'scissors') || (player == 'scissors' && computer == 'rock')
+  elsif win?(computer)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
